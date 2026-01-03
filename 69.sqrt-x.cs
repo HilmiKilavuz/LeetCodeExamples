59c663a05ccbe79ee33cf02021d535111185e0ec
HilmiKilavuz <kilavuzhilmi@gmail.com>
2026-01-04 00:29:58 +0300

    69 çözüldü

---
+++ 69.sqrt-x.cs
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=69 lang=csharp
+ *
+ * [69] Sqrt(x)
+ */
+
+// @lc code=start
+public class Solution {
+    public int MySqrt(int x) {
+
+        int result = (int)Math.Floor(Math.Sqrt(x));
+        return result;
+        
+    }
+}
+// @lc code=end
+

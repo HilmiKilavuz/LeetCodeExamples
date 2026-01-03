37bfabd8abbe678e2747bc0d0199eaca84d111ee
HilmiKilavuz <kilavuzhilmi@gmail.com>
2026-01-03 23:48:28 +0300

    232 çözüldü

---
+++ 232.implement-queue-using-stacks.cs
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=232 lang=csharp
+ *
+ * [232] Implement Queue using Stacks
+ */
+
+// @lc code=start
+public class MyQueue {
+    Stack<int> input;
+    Stack<int> output;
+    public MyQueue() {
+        input = new Stack<int>();
+        output = new Stack<int>();
+
+    }
+    
+    public void Push(int x) {
+        input.Push(x);
+        
+    }
+    
+    public int Pop() {
+
+        if (output.Count==0)    
+        {
+            while (input.Count!=0)
+        {
+            output.Push(input.Pop());
+        }
+            
+        }
+        
+       return output.Pop();
+
+
+    }
+    
+    public int Peek() {
+        
+        if (output.Count==0)    
+        {
+            while (input.Count!=0)
+        {
+            output.Push(input.Pop());
+        }
+            
+        }       
+        return output.Peek();
+    
+    }
+    
+    public bool Empty() {
+        if (input.Count<=0 && output.Count<=0)
+        {
+           return true; 
+        }
+        return false;
+        
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
+// @lc code=end
+
